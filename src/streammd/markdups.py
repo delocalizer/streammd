241a2e631fd5a9dec1369e12de7098ad932e19bf
delocalizer <conrad.leonard@hotmail.com>
2022-08-15 21:47:06 +1000

    use process Pool instead of manually starting Process instances

--- src/streammd/markdups.py
+++ src/streammd/markdups.py
@@ -8,8 +8,8 @@ Currently only paired reads are handled.
 Default log level is 'INFO' — set to something else with the LOG_LEVEL
 environment variable.
 """
-from concurrent.futures import ProcessPoolExecutor
-from multiprocessing import Queue, Process
+from itertools import repeat
+from multiprocessing import Manager, Pool, Process
 from multiprocessing.managers import SharedMemoryManager
 import argparse
 import logging
@@ -27,9 +27,10 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
 LOGGER.addHandler(logging.StreamHandler())
 
-MSG_COUNT = 'qnames seen: %s'
-MSG_UNIQUE = 'approximate count of unique pairs: %s'
-MSG_DUPLICATES = 'approximate duplicate fraction: %0.4f'
+MSG_NQNAME = 'qnames seen: %s'
+MSG_NUNIQUE = 'approximate count of unique pairs: %s'
+MSG_NDUP = 'approximate count of duplicates: %s'
+MSG_DUPFRAC = 'approximate duplicate fraction: %0.4f'
 SENTINEL = 'STOP'
 
 
@@ -52,7 +53,6 @@ def samrecords(headerq, samq, nconsumers, batchsize=50, infd=0, outfd=1):
     Returns:
         None
     """
-    count = 0
     group = []
     groupid = None
     header = None
@@ -74,18 +74,15 @@ def samrecords(headerq, samq, nconsumers, batchsize=50, infd=0, outfd=1):
             else:
                 if group:
                     batch.append(group)
-                    count += 1
                     if len(batch) == batchsize:
                         samq.put(batch)
                         batch = []
                 groupid = qname
                 group = [record]
     batch.append(group)
-    count += 1
     samq.put(batch)
     for _ in range(nconsumers):
         samq.put(SENTINEL)
-    samq.put(count)
 
 
 def markdups(bfconfig, headerq, samq, outfd=1):
@@ -99,25 +96,29 @@ def markdups(bfconfig, headerq, samq, outfd=1):
         outfd: output stream file descriptor (default=1).
 
     Returns:
-        None
+        (n_qname, n_dupe): number of qnames and number of duplicates seen.
     """
     bf = BloomFilter.copy(bfconfig)
     header = AlignmentHeader.from_text(headerq.get())
+    n_qname, n_dup = 0, 0
     while True:
         batch = samq.get()
         if batch == SENTINEL:
             break
         for group in batch:
+            n_qname += 1
             alignments = [AlignedSegment.fromstring(r, header) for r in group]
             ends = readends(alignments)
             if ends and bf.add(ends):
+                n_dup += 1
                 for a in alignments:
                     a.flag += 1024
             # Write the group as a group. In contrast to sys.stdout.write,
             # os.write is atomic so we don't have to care about locking or
             # using an output queue.
             out = '\n'.join(a.to_string() for a in alignments) + '\n'
-            os.write(outfd, out.encode('utf-8'))
+            os.write(outfd, out.encode('ascii'))
+    return (n_qname, n_dup)
 
 
 def readends(alignments):
@@ -135,21 +136,21 @@ def readends(alignments):
                 r1 = alignment
             elif alignment.is_read2:
                 r2 = alignment
+    ends = [None, None]
     # TODO: allow single ends to be mapped
     if r1.is_unmapped or r2.is_unmapped:
         return None
-    ends = [None, None]
     for i, r in enumerate((r1, r2)):
         if r.is_forward:
-            leading_S = r.cigar[0][1] if r.cigar[0][0] == 4 else 0
+            front_s = r.cigar[0][1] if r.cigar[0][0] == 4 else 0
                                                           # leading soft clips
-            ends[i] = r.reference_name, r.reference_start - leading_S
+            ends[i] = r.reference_name, r.reference_start - front_s
         else:
-            trailing_S = r.cigar[-1][1] if r.cigar[-1][0] == 4 else 0
+            back_s = r.cigar[-1][1] if r.cigar[-1][0] == 4 else 0
                                                         # trailing soft clips
-            ends[i] = r.reference_name, r.reference_end + trailing_S
+            ends[i] = r.reference_name, r.reference_end + back_s
     orientation = '^' if r1.is_reverse ^ r2.is_reverse else '='
-    # define canonical ordering: l < r
+    # canonical ordering: l < r
     ends.sort()
     (l_rname, l_pos), (r_rname, r_pos) = ends
     return f'{orientation}{l_rname}{l_pos}{r_rname}{r_pos}'
@@ -188,25 +189,24 @@ def main():
     Run as CLI script
     """
     args = parse_cmdargs(sys.argv[1:])
-    headerq = Queue(args.consumer_processes)
-    samq = Queue(args.queue_size)
-    producer = Process(target=samrecords,
-                       args=(headerq, samq, args.consumer_processes))
+    LOGGER.info(' '.join(sys.argv))
+    manager = Manager()
+    headerq = manager.Queue(args.consumer_processes)
+    samq = manager.Queue(args.queue_size)
+    nconsumers = args.consumer_processes
+    producer = Process(target=samrecords, args=(headerq, samq, nconsumers))
     producer.start()
-    with SharedMemoryManager() as smm:
+    with SharedMemoryManager() as smm, Pool(nconsumers) as pool:
         bf = BloomFilter(smm, args.n_items, args.fp_rate)
-        consumers = [
-            Process(target=markdups, args=(bf.config, headerq, samq))
-            for _ in range(args.consumer_processes)
-        ]
-        list(map(lambda x: x.start(), consumers))
-        list(map(lambda x: x.join(), consumers))
-        n_qname = samq.get()
+        mdargs = repeat((bf.config, headerq, samq), nconsumers)
+        counts = pool.starmap(markdups, mdargs)
+        n_qname, n_dup = [sum(col) for col in zip(*counts)]
         n_unique = bf.count()
         producer.join()
-    LOGGER.info(MSG_COUNT % n_qname)
-    LOGGER.info(MSG_UNIQUE % n_unique)
-    LOGGER.info(MSG_DUPLICATES % ((n_qname - n_unique)/n_qname))
+    LOGGER.info(MSG_NQNAME, n_qname)
+    LOGGER.info(MSG_NUNIQUE, n_unique)
+    LOGGER.info(MSG_NDUP, n_dup)
+    LOGGER.info(MSG_DUPFRAC, (n_dup/n_qname))
 
 
 if __name__ == '__main__':
