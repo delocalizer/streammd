d897733e694cfcd84e846333bca1cd08b5846702
delocalizer <conrad.leonard@hotmail.com>
2022-09-23 16:08:20 +1000

    some IDE stuff

--- src/bloomfilter.cxx
+++ src/bloomfilter.cxx
@@ -1,7 +1,7 @@
 #include <boost/dynamic_bitset.hpp>
 #include <iostream>
 #include <math.h>
-#include <../libs/xxhash_cpp/include/xxhash.hpp>
+#include "../libs/xxhash_cpp/include/xxhash.hpp"
 
 int main(int argc, char* argv[]) {
     // initializes all to 0
@@ -10,6 +10,10 @@ int main(int argc, char* argv[]) {
     B1.set(0);
     std::cout << "test(0) after set: " << B1.test(0) << std::endl;
     std::array<int, 4> input {322, 2137, 42069, 65536};
-    xxh::hash_t<64> hash = xxh::xxhash3<64>(input); 
-    std::cout << "hash of input is " << hash << std::endl;
+    uint64_t seed1 = 41;
+    uint64_t seed2 = 43;
+    xxh::hash_t<64> hash1 = xxh::xxhash3<64>(input, seed1);
+    xxh::hash_t<64> hash2 = xxh::xxhash3<64>(input, seed2);
+    std::cout << "hash1 of input is " << hash1 << std::endl;
+    std::cout << "hash1 of input is " << hash2 << std::endl;
 }
