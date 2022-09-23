7f582a7809123d2b4b1c63cfc8109f6427636d4c
delocalizer <conrad.leonard@hotmail.com>
2022-09-23 15:51:07 +1000

    use 64bit xxhash3

--- src/bloomfilter.cxx
+++ src/bloomfilter.cxx
@@ -10,6 +10,6 @@ int main(int argc, char* argv[]) {
     B1.set(0);
     std::cout << "test(0) after set: " << B1.test(0) << std::endl;
     std::array<int, 4> input {322, 2137, 42069, 65536};
-    xxh::hash_t<32> hash = xxh::xxhash<32>(input); 
+    xxh::hash_t<64> hash = xxh::xxhash3<64>(input); 
     std::cout << "hash of input is " << hash << std::endl;
 }
