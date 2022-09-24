51997583795d9548eca21cf0cbd67a161aea9d30
delocalizer <conrad.leonard@hotmail.com>
2022-09-24 15:05:28 +1000

    bloomfilter as a lib

--- src/bloomfilter.cxx
+++ src/bloomfilter.cxx
@@ -1,19 +1,17 @@
-#include <iostream>
-#include <math.h>
-#include <boost/dynamic_bitset/dynamic_bitset.hpp>
-#include <xxhash_cpp/include/xxhash.hpp>
+#include <cstdint>
+#include <string>
+#include <tuple>
+#include "bloomfilter.h"
 
-int main(int argc, char* argv[]) {
-    // initializes all to 0
-    boost::dynamic_bitset<> b1(pow(1024, 2));
-    std::cout << "test(0) before set: " << b1.test(0) << std::endl;
-    b1.set(0);
-    std::cout << "test(0) after set: " << b1.test(0) << std::endl;
-    std::array<int, 4> input {322, 2137, 42069, 65536};
-    uint64_t seed1 = 41;
-    uint64_t seed2 = 43;
-    xxh::hash_t<64> hash1 = xxh::xxhash3<64>(input, seed1);
-    xxh::hash_t<64> hash2 = xxh::xxhash3<64>(input, seed2);
-    std::cout << "hash1 of input is " << hash1 << std::endl;
-    std::cout << "hash1 of input is " << hash2 << std::endl;
+using namespace bloomfilter;
+
+BloomFilter::BloomFilter(uint64_t n, float p):  n_(n), p_(p) {
+}
+
+bool BloomFilter::operator&(const std::string&) {
+  return false;
+}
+
+bool BloomFilter::operator|=(const std::string&) {
+  return false;
 }
