6862f4611c9b676fa99e7fbdc248e9c067eb2e2b
delocalizer <conrad.leonard@hotmail.com>
2022-09-25 23:28:49 +1000

    add Boost logging

--- src/bloomfilter.cxx
+++ src/bloomfilter.cxx
@@ -4,6 +4,7 @@
 #include <tuple>
 
 #include <boost/dynamic_bitset/dynamic_bitset.hpp>
+#include <boost/log/trivial.hpp>
 #include <xxhash_cpp/xxhash.hpp>
 
 #include "bloomfilter.h"
@@ -15,6 +16,8 @@ BloomFilter::BloomFilter(uint64_t n, float p):  n_{n}, p_{p} {
   std::tie(m_, k_) = m_k_min(n_, p_);
   // dynamic_bitset constructor initializes all to 0
   bitset = std::unique_ptr<boost::dynamic_bitset<>>(new boost::dynamic_bitset(m_));
+  BOOST_LOG_TRIVIAL(info) << "BloomFilter intialized with" <<
+    " n=" << n_ << " p=" << p_ << " m=" << m_ << " k=" << k_;
 }
 
 // Check if item is present.
