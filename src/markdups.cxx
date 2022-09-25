0cc8db6851fd6b6aa2f5a96806b9ac20aa63ca7a
delocalizer <conrad.leonard@hotmail.com>
2022-09-25 21:17:52 +1000

    more CLI config

--- src/markdups.cxx
+++ src/markdups.cxx
@@ -14,15 +14,35 @@ int main(int argc, char* argv[]) {
 
   argparse::ArgumentParser cli("streammd", STREAMMD_VERSION);
 
+  cli.add_description(
+    "Read a SAM file from STDIN, mark duplicates in a single pass and stream "
+    "processed records to STDOUT. Input must begin with a valid SAM header "
+    "followed by qname-grouped records. Default log level is 'INFO' — set to "
+    "something else with the LOG_LEVEL environment variable.");
+
   cli.add_argument("-n", "--n-items")
     .help("Expected maximum number of templates n.")
     .default_value(default_n)
+    .metavar("N_ITEMS")
     .scan<'d', uint64_t>();
 
   cli.add_argument("-p", "--fp-rate")
     .help("Target maximum false positive rate when n items are stored.")
     .default_value(default_p)
+    .metavar("FP_RATE")
     .scan<'g', float>();
+
+  cli.add_argument("--single")
+    .help("Accept single-ended reads as input. [default: paired-end]")
+    .default_value(false)
+    .implicit_value(true);
+
+  cli.add_argument("--strip-previous")
+    .help("Unset duplicate flag for any reads that have it set and are no "
+          "longer considered duplicate. Only ever required if records have "
+          "previously been through a duplicate marking step. [default: false]")
+    .default_value(false)
+    .implicit_value(true);
 
   try {
     cli.parse_args(argc, argv);
@@ -34,12 +54,7 @@ int main(int argc, char* argv[]) {
 
   auto n { cli.get<uint64_t>("-n") };
   auto p { cli.get<float>("-p") };
-  std::cout << "n: " << n << " p: " << p << std::endl;
-  bloomfilter::BloomFilter bf(n, p);
-  std::string item { "foo bar baz" };
-  std::cout << "bf.contains(item) before add: " << bf.contains(item) << std::endl;
-  bf.add(item); 
-  std::cout << "bf.contains(item) after add: " << bf.contains(item) << std::endl;
-  std::cout << "count_estimate: " << bf.count_estimate() << std::endl;
+  auto reads_per_template { cli.get<bool>("--single") ? 1 : 2 };
+
 }
 
