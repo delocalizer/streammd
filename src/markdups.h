6afd234a4f80ed2156c1b41fd2a29e5fe72ab83d
delocalizer <conrad.leonard@hotmail.com>
2022-09-25 20:36:36 +1000

    rename

--- src/markdups.h
+++ src/markdups.h
@@ -1,12 +1,12 @@
-#ifndef STREAMMD_STREAMMD_H_
-#define STREAMMD_STREAMMD_H_
+#ifndef STREAMMD_MARKDUPS_H_
+#define STREAMMD_MARKDUPS_H_
 
 #include <cstdint>
 #include <string>
 
-namespace streammd {
+namespace markdups {
   const uint64_t default_n { 1000000000 };
   const float default_p { 0.000001 };
 }
 
-#endif // STREAMMD_STREAMMD_H_
+#endif // STREAMMD_MARKDUPS_H_
