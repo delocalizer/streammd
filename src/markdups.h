b68cc7a0a853841a261ffc22c59b7fcc14283163
delocalizer <conrad.leonard@hotmail.com>
2022-09-26 15:32:12 +1000

    sketch of major structure done

--- src/markdups.h
+++ src/markdups.h
@@ -8,6 +8,7 @@ namespace markdups {
   const uint64_t default_n { 1000000000 };
   const float default_p { 0.000001 };
   const std::string default_metrics { "streammd-metrics.json" };
+  const uint32_t log_interval { 1000000 };
   const char SAM_delimiter { '\t' };
 }
 
