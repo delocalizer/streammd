624b8213a9030734f7e9bf4ff012c80e00dd4966
delocalizer <conrad.leonard@hotmail.com>
2022-10-03 14:02:31 +1000

    tests

--- tests/test_bloomfilter.cxx
+++ tests/test_bloomfilter.cxx
@@ -10,6 +10,35 @@
 
 using namespace bloomfilter;
 
-TEST_CASE(" m_k_min ", "[bloomfilter]") {
-  REQUIRE(BloomFilter::m_k_min(1000, 0.001) == std::make_tuple(14378,	10));
+TEST_CASE("m_k_min", "[static]") {
+  REQUIRE(BloomFilter::m_k_min(1000000, 0.000001) == std::make_tuple(28755177, 20));
+  REQUIRE(BloomFilter::m_k_min(10000000, 0.0000001) == std::make_tuple(335477051,	24));
+  REQUIRE(BloomFilter::m_k_min(100000000, 0.00000001) == std::make_tuple(3834023396, 27));
+  REQUIRE(BloomFilter::m_k_min(1000000000, 0.000001) == std::make_tuple(28755176136,	20));
+}
+
+TEST_CASE("BloomFilter::add missing", "[functionality]") {
+  BloomFilter bf(1000, 0.001);
+  auto key = "something";
+  REQUIRE(bf.add(key) == true);
+}
+
+TEST_CASE("BloomFilter::add existing", "[functionality]") {
+  BloomFilter bf(1000, 0.001);
+  auto key = "something";
+  bf.add(key);
+  REQUIRE(bf.add(key) == false);
+}
+
+TEST_CASE("BloomFilter::contains missing", "[functionality]") {
+  BloomFilter bf(1000, 0.001);
+  auto key = "something";
+  REQUIRE(bf.contains(key) == false); 
+}
+
+TEST_CASE("BloomFilter::contains existing", "[functionality]") {
+  BloomFilter bf(1000, 0.001);
+  auto key = "something";
+  bf.add(key);
+  REQUIRE(bf.contains(key) == true);
 }
